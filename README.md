1ea4d7197c9dc510f9d426847538959ffa13075c
Josh Curtis <joshc@Joshs-MacBook-Pro.local>
2024-07-18 00:46:34 +0100

    complete technical task using artillery

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sainsbury's Tech Exercise
+
+This project contains a load test for adding a product to a shopping basket. It's designed to ensure our basket functionality can handle high traffic efficiently.
+
+## Prerequisites
+
+- Node.js (tested on v22)
+- Yarn or npm
+
+## Installation
+
+Follow these steps to set up the project on your local machine:
+
+1. Clone the repository to your local machine.
+2. Navigate to the project directory.
+3. Run `yarn` to install all the necessary dependencies.
+
+## Running the Test
+
+To start the load test, follow these steps:
+
+1. Ensure you are in the project root directory.
+2. Run `yarn test` to initiate the test.
+3. A `results.json` file will be created containing the test output
+4. A `results.json.html` file is generated with a visual report (this comand is deprecated and replaced with their Cloud offering but a simple way to view results for this exercise).
---
+++ tests/performance-scripts.ts
@@ -0,0 +1,48 @@
+import { test, expect, Page } from '@playwright/test';
+
+export async function atb(
+    page: Page, userContext, events, test
+) {
+    await test.step('ATB - digestive biscuits', async () => {
+        // Nav to home page and accept cookies
+        await page.goto('https://www.sainsburys.co.uk/');
+        await page.getByRole('button', { name: 'Accept all cookies' }).click();
+
+        // Search for digestive biscuits
+        await page.getByTestId('search-term').click();
+        await page.getByTestId('search-term').fill('digestive biscuits');
+        await page.getByTestId('search-btn').click();
+
+        // Click on the first product in the search results
+        const firstProductTile = await page.locator('[data-test-id^="product-tile-"]').first();
+        await firstProductTile.locator('.pt__link').click();
+
+        // Try to add the product to the trolley
+        await page.locator('[data-test-id="pd-control-buttons"] [data-test-id="add-button"]').click();
+
+        // Click on the accept cookies button again
+        await page.getByRole('button', { name: 'Accept all cookies' }).click();
+
+        // Login to our test account
+        await page.getByTestId('username').click();
+        await page.getByTestId('username').fill('tahimel927@ikangou.com');
+
+        await page.getByTestId('password').click();
+        await page.getByTestId('password').fill('SainsburysTest1!');
+
+        await page.getByTestId('log-in').click();
+
+        // Add the product to the trolley, or increase the quantity if it's already in the trolley
+        await page.locator('[data-test-id="pd-control-buttons"] [data-test-id="add-button"]')
+            .or(page.locator('[data-test-id="pt-button-inc"]'))
+            .click();
+
+        // Go to the trolley page
+        await page.locator('[data-test-id="header-trolley"]').click();
+
+        // Check the title of the page is correct
+        await expect(page).toHaveTitle(/Trolley | Sainsbury's/);
+        await expect(page.locator('[data-test-id="trolley-item"]')).toBeVisible();
+        await page.screenshot({ path: `trolley-${Date.now()}.png` });
+    });
+}
